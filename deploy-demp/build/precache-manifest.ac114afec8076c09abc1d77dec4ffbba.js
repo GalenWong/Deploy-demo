7e9a43ff761ec94ca0c53c27feab5b3af707f6a3
GalenWong <wonggalen1999@gmail.com>
2018-11-27 23:08:40 -0800

    react

---
+++ deploy-demp/build/precache-manifest.ac114afec8076c09abc1d77dec4ffbba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9c6ac0dc864d3fd7bd1",
+    "url": "/static/css/main.d886ce5a.chunk.css"
+  },
+  {
+    "revision": "f9c6ac0dc864d3fd7bd1",
+    "url": "/static/js/main.f9c6ac0d.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "86ba0edbfea326b81b74298de07c7835",
+    "url": "/index.html"
+  }
+];
